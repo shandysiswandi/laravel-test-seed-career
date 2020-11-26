c33b4420637954f7fc887336e93e4dbfa206537e
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-26 16:40:02 +0700

    implement logger

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,42 +6,72 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\GetUserRequest;
 use App\Services\UserService;
 use App\Traits\ApiResponse;
+use App\Traits\Logger;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    use ApiResponse;
+    use ApiResponse, Logger;
 
     public function getUser(GetUserRequest $request, UserService $userService)
     {
-        $user = $userService->findByID($request->id);
+        // log request
+        $this->logRequest($request, ['id' => $request->id]);
+
+        $user = $userService->findByIDWithCompany($request->id);
 
         if (!$user) {
-            return $this->apiError("User Not Found", 404);
+            $response = $this->apiError("User Not Found", 404);
+
+            // log response
+            $this->logResponse($request, $response);
+
+            return $response;
         }
 
-        return $this->apiSuccess("Success get user", [
+        $response = $this->apiSuccess("Success get user", [
             'id' => $user->id,
+            'company' => $user->company->name,
             'fullName' => "$user->first_name $user->last_name",
             'email' => $user->email,
             'account' => $user->account
         ]);
+
+        // log response
+        $this->logResponse($request, $response);
+
+        return $response;
     }
 
-    public function getListUser(UserService $userService)
+    public function getListUser(Request $request, UserService $userService)
     {
-        $users = $userService->listUser();
+        // log request
+        $this->logRequest($request);
+
+        $users = $userService->listUserWithCompany();
 
         if ($users->isEmpty()) {
-            return $this->apiSuccess("Success but users empty", $users);
+            $response = $this->apiSuccess("Success but users empty", $users);
+
+            // log response
+            $this->logResponse($request, $response);
+
+            return $response;
         }
 
-        return $this->apiSuccess("Success get users", $users->map(function ($item) {
+        $response = $this->apiSuccess("Success get users", $users->map(function ($item) {
             $res['id'] = $item->id;
+            $res['company'] = $item->company->name;
             $res['fullName'] = "$item->first_name $item->last_name";
             $res['email'] = $item->email;
             $res['account'] = $item->account;
 
             return $res;
         }));
+
+        // log response
+        $this->logResponse($request, $response);
+
+        return $response;
     }
 }
