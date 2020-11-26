619cc3a977aff700552c504e0cdfc0e45524103e
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-26 20:43:18 +0700

    add method createUser

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -6,6 +6,13 @@ use App\Models\User;
 
 class UserService
 {
+    public $companyService;
+
+    public function __construct(CompanyService $companyService)
+    {
+        $this->companyService = $companyService;
+    }
+
     public function findByIDWithCompany($id)
     {
         return User::with('company')->find($id);
@@ -14,5 +21,16 @@ class UserService
     public function listUserWithCompany()
     {
         return User::with('company')->get();
+    }
+
+    public function createUser($request)
+    {
+        return User::create([
+            'company_id' => $request->company_id,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'account' => "" . rand(11111111, 99999999),
+        ]);
     }
 }
