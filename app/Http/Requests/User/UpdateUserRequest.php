fcd69467758bb4efad5599d61099ccac15f19733
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-26 22:19:36 +0700

    create UpdateUserRequest

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'first_name' => 'nullable|string|min:3|max:100',
+            'last_name' => 'nullable|string|min:3|max:100',
+            'email' => 'nullable|email|unique:users,email,' . request()->id . '|max:100',
+        ];
+    }
+}
