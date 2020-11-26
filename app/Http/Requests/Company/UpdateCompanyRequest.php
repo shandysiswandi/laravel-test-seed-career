b368acd30013316150fa52ccf758a65bd867b906
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-26 22:37:03 +0700

    create UpdateCompanyRequest

---
+++ app/Http/Requests/Company/UpdateCompanyRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Company;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompanyRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'nullable|string|min:3|max:255',
+            'address' => 'nullable|string|min:3|max:100',
+        ];
+    }
+}
