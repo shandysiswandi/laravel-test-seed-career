a5083581af806e64a06f3238017bd7b8986d61c5
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-26 17:22:15 +0700

    implement logger

--- app/Http/Controllers/Api/CompanyBudgetController.php
+++ app/Http/Controllers/Api/CompanyBudgetController.php
@@ -6,30 +6,62 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CompanyBudget\GetCompanyBudgetRequest;
 use App\Services\CompanyBudgetService;
 use App\Traits\ApiResponse;
+use App\Traits\Logger;
+use Illuminate\Http\Request;
 
 class CompanyBudgetController extends Controller
 {
-    use ApiResponse;
+    use ApiResponse, Logger;
 
     public function getCompanyBudget(GetCompanyBudgetRequest $request, CompanyBudgetService $companyBudgetService)
     {
-        $companyBudget = $companyBudgetService->findByID($request->id);
+        // log request
+        $this->logRequest($request, ['id' => $request->id]);
+
+        $companyBudget = $companyBudgetService->findByIDWithCompany($request->id);
 
         if (!$companyBudget) {
+            $this->logResponse($request, 'Company Budget Not Found');
+
             return $this->apiError("Company Budget Not Found", 404);
         }
 
-        return $this->apiSuccess("Success get company budget", $companyBudget);
+        $data = [
+            'id' => $companyBudget->id,
+            'company' => $companyBudget->company->name,
+            'amount' => $companyBudget->amount
+        ];
+
+        // log response
+        $this->logResponse($request, $data);
+
+        return $this->apiSuccess("Success get company budget", $data);
     }
 
-    public function getListCompanyBudget(CompanyBudgetService $companyBudgetService)
+    public function getListCompanyBudget(Request $request, CompanyBudgetService $companyBudgetService)
     {
-        $companyBudgets = $companyBudgetService->listCompanyBudget();
+        // log request
+        $this->logRequest($request);
+
+        $companyBudgets = $companyBudgetService->listCompanyBudgetWithCompany();
 
         if ($companyBudgets->isEmpty()) {
+            $this->logResponse($request, 'Success but company budgets empty');
+
             return $this->apiSuccess("Success but company budgets empty", $companyBudgets);
         }
 
-        return $this->apiSuccess("Success get company budgets", $companyBudgets);
+        $data = $companyBudgets->map(function ($item) {
+            $res['id'] = $item->id;
+            $res['company'] = $item->company->name;
+            $res['amount'] = $item->amount;
+
+            return $res;
+        });
+
+        // log response
+        $this->logResponse($request, $data);
+
+        return $this->apiSuccess("Success get company budgets", $data);
     }
 }
