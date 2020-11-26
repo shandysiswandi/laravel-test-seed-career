10a7320a0f60a5a0011cebe7198804e5a057fc2b
Shandy Siswandi <shandysiswandi@gmail.com>
2020-11-27 00:20:34 +0700

    add method findByCompanyId

--- app/Services/CompanyBudgetService.php
+++ app/Services/CompanyBudgetService.php
@@ -11,6 +11,11 @@ class CompanyBudgetService
         return CompanyBudget::with('company')->find($id);
     }
 
+    public function findByCompanyId($company_id)
+    {
+        return CompanyBudget::where('company_id', $company_id)->first();
+    }
+
     public function listCompanyBudgetWithCompany()
     {
         return CompanyBudget::with('company')->get();
